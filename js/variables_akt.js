4cee3cb16d1c7a6c9b910fa1e950153c85e9d25c
geodata-Sandnes <geodata.appkonto@sandnes.kommune.no>
2024-08-13 08:34:48 +0200

    Update variables_akt.js

--- js/variables_akt.js
+++ js/variables_akt.js
@@ -1,11 +1,6 @@
 const jsonUrl = "json/aktiviteter.json";
 const direct = "https://geodata-Sandnes.github.io/Web-kart/";
 const grunnkart = "https://services.geodataonline.no/arcgis/rest/services/Geocache_WMAS_WGS84/GeocacheBasis/MapServer/tile/{z}/{y}/{x}";
-
-
-
-
-
 const attribution_tag = '©️ Geodata AS, Kartverket, Geovekst og kommunene, OpenStreetMap';
 const marker_blaa = "css/images/icon/marker-icon_blaa.png";
 const testtekst = "Var test ok";
